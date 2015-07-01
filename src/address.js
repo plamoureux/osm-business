c95fe6afc67ca1a3c6ab23c1ab4ea90391cb71a6
plamoureux <plamoureux@octopus-itsm.com>
2015-07-01 15:07:32 -0400

    Delete address.js

--- src/address.js
+++
@@ -1,9 +0,0 @@
-import {Entity, field} from 'skyy/entity';
-
-@field('building', 'string', {caption: 'Édifice'})
-@field('street', 'string', {caption: 'Rue'})
-@field('city', 'string', {caption: 'Ville'})
-@field('postalCode', 'string', {caption: 'Code postal'})
-@field('state', 'lookup', {caption: 'Province', items: {Québec: 'Québec', Ontario: 'Ontario'}})
-@field('country', 'lookup', {caption: 'Pays', items: {Canada: 'Canada', France: 'France'}})
-export class Address extends Entity {}
